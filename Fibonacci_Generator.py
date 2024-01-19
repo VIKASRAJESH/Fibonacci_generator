398ea5ee37659820cbd013941568ba14710d6464
VIKASRAJESH <153272233+VIKASRAJESH@users.noreply.github.com>
2024-01-19 14:41:51 +0530

    Add files via upload

---
+++ Fibonacci_Generator.py
@@ -0,0 +1,15 @@
+def fibonacci_sequence():
+    n=input("Enter How many Fibonaccis to generate?(n):")
+    n=int(n)
+    a=0
+    b=1
+    if n < 0:
+        print("Invalid 'n' value.")
+    elif n == 0:
+        print(0)
+    else:
+        for i in range(n):
+            print(b)
+            a,b= b,a+b
+ 
+fibonacci_sequence()
